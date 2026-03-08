8c327a4497bda4f7514e5aa24b2be36c8c1ec5e0
mhemmingsen1 <michaelhemmingsen@gmail.com>
2026-03-08 17:40:22 +0800

    fix base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://icic-conference.github.io',
+  base: '/icic-site',
+});
