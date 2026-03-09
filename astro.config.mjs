c48bc7a3d21bdabebbd5e6f9a9903b589dd99bc2
mhemmingsen1 <michaelhemmingsen@gmail.com>
2026-03-09 14:35:01 +0800

    make it live actually

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://icic-conference.github.io',
-  export default defineConfig({
-  site: 'https://icic.thu.edu.tw',
-});
+  base: '/icic-site',
 });
