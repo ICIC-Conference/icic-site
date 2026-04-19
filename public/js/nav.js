917a5d668b40085ea4902401d727e0553d0fbfbf
mhemmingsen1 <michaelhemmingsen@gmail.com>
2026-04-19 16:38:28 +0800

    hamburger

---
+++ public/js/nav.js
@@ -0,0 +1,5 @@
+const hamburger = document.getElementById('hamburger');
+const nav = document.getElementById('main-nav');
+hamburger.addEventListener('click', () => {
+  nav.classList.toggle('open');
+});
